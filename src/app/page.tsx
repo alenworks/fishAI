2ae1e8738a704316dde2ee841e2b2a907098c689
lxj <13567481362@163.com>
2024-12-03 16:06:06 +0800

    feat(project): 项目初始lint格式配置,添加包

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+import { Button } from '@/components/ui/button'
+import HomeNav from '@/components/HomeNav'
+import { Zap } from 'lucide-react'
 export default function Home() {
   return (
-    <div >
-   
-    </div>
-  );
+    <main className="min-h-screen flex flex-col justify-center text-center">
+      <HomeNav />
+      <h2 className="scroll-m-20 text-4xl tracking-tight lg:text-5xl">
+        <span className="font-extrabold">双鱼 AI</span>，智启笔端，创意无限
+      </h2>
+      <p className="leading-7 [&:not(:first-child)]:mt-6 text-lg">
+        <span>
+          基于 GPT 模型，AI 写作，AI 智能提示，AI 文本处理，多人协同编辑
+        </span>
+      </p>
+      <section className="mt-10 flex justify-center space-x-4">
+        <Button className="text-base" size="lg">
+          <Zap className="h-4 w-4" />
+          &nbsp;开始使用
+        </Button>
+      </section>
+    </main>
+  )
 }
---
+++ src/components/logo.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+export default function Logo() {
+  return (
+    <Link href="/" className="blog w-30 overflow-hidden">
+      <h1 className="font-bold text-xl bg-muted w-32">
+        &nbsp;双鱼&nbsp;
+        <span className="text-background bg-foreground">
+          &nbsp;AI写作&nbsp;
+        </span>
+      </h1>
+    </Link>
+  )
+}
