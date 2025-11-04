31d0bd6373192009e074f379ddab3bebdf81a715
“alenworks” <1274657521@qq.com>
2025-11-04 11:05:55 +0800

    chore: 加入react-compiler 优化组件

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com'], // 添加头像域名
   },
+  experimental: {
+    reactCompiler: true,
+  },
+  reactStrictMode: true,
 }
 
 export default nextConfig
